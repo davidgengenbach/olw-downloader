7663d39d42d85f5c37ceb6bb211860e4908eb894
David Gengenbach <davidgengenbach@gmx.de>
2015-04-16 02:06:14 +0200

    :)

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
-var API_URL = "https://openlearnware.tu-darmstadt.de/olw-rest-db/api/";
-
 var config = {
-    DOWNLOAD_FOLDER: 'downloaded/',
-    VIDEO_EXTENSION: '.mp4',
-    VIDEO_API_URL: API_URL + 'resource-detailview/index/',
-    COLLECTION_API_URL: API_URL + 'collection-detailview/index/',
+    DOWNLOAD_FOLDER:    'downloaded/',
+    VIDEO_EXTENSION:    '.mp4',
+    VIDEO_API_URL:      'https://openlearnware.tu-darmstadt.de/olw-rest-db/api/resource-detailview/index/',
+    COLLECTION_API_URL: 'https://openlearnware.tu-darmstadt.de/olw-rest-db/api/collection-detailview/index/',
     VIDEO_DOWNLOAD_URL: 'https://olw-material.hrz.tu-darmstadt.de/olw-konv-repository/material/'
 };
 
